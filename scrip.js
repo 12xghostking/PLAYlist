259ed93db0fdeb192eef1e433fa1c0715e019e23
12xghostking <78692734+12xghostking@users.noreply.github.com>
2021-05-28 19:36:09 +0530

    added script

---
+++ scrip.js
@@ -0,0 +1,13 @@
+function myFunction()
+{
+  document.getElementById("r").style.color = "rgb(36, 0, 241)";
+  document.getElementById("s").style.color = "rgb(36, 0, 241)";
+  document.getElementById("t").style.color = "rgb(36, 0, 241)";
+  document.getElementById("u").style.color = "rgb(36, 0, 241)";
+  document.getElementById("v").style.color = "rgb(36, 0, 241)";
+  document.getElementById("r").style.textAlign = "center";
+  document.getElementById("s").style.textAlign = "center";
+  document.getElementById("t").style.textAlign = "center";
+  document.getElementById("u").style.textAlign = "center";
+  document.getElementById("v").style.textAlign = "center";
+}
