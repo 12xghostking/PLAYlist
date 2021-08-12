eac38975c5c8ae373fa7df00e2d7924d7dc26ed5
12xghostking <78692734+12xghostking@users.noreply.github.com>
2021-08-12 17:35:26 +0530

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -5,9 +5,6 @@ function myFunction()
   document.getElementById("t").style.color = "rgb(36, 0, 241)";
   document.getElementById("u").style.color = "rgb(36, 0, 241)";
   document.getElementById("v").style.color = "rgb(36, 0, 241)";
-  document.getElementById("r").style.textAlign = "center";
-  document.getElementById("s").style.textAlign = "center";
-  document.getElementById("t").style.textAlign = "center";
-  document.getElementById("u").style.textAlign = "center";
-  document.getElementById("v").style.textAlign = "center";
+  document.getElementById("t1").style.textAlign = "center";
+ 
 }
