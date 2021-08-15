2298829197d419fb61e121c85895fceb6f7e8642
12xghostking <78692734+12xghostking@users.noreply.github.com>
2021-08-15 15:28:45 +0530

    bg and effects

--- scrip.js
+++ scrip.js
@@ -6,5 +6,5 @@ function myFunction()
   document.getElementById("u").style.color = "rgb(36, 0, 241)";
   document.getElementById("v").style.color = "rgb(36, 0, 241)";
   document.getElementById("t1").style.textAlign = "center";
- 
+  document.getElementById("t1").style.fontFamily = "Audiowide"
 }
